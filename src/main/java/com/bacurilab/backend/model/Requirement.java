ab2381d0f9947a4e426dc04b1184bde8b1bea9f6
jhersonharyson <jherson_k-f@hotmail.com>
2020-12-17 23:19:17 -0300

    --refactor sql to dont do alter table for owner postgres

--- src/main/java/com/bacurilab/backend/model/Requirement.java
+++ src/main/java/com/bacurilab/backend/model/Requirement.java
@@ -3,7 +3,8 @@ package com.bacurilab.backend.model;
 public enum Requirement {
     MIDDLE_AGE_WOMEN("MIDDLE_AGE_WOMEN"),
     GIRL("GIRL"),
-    BOY("BOY");
+    BOY("BOY"),
+    MANDATORY("MANDATORY");
 
     Requirement(String requirement) {
     }
